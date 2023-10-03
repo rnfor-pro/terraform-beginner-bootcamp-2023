a111ce27fc437aae25a02a29ea1f5566804a2f6d
rnfor-pro <nfor.rudolph1@gmail.com>
2023-10-03 01:52:52 +0000

    #9 add terraform random provider

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+  lower = true
+  numeric = false
+  override_special = ""
+}
+
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
+
+
+
+
 
