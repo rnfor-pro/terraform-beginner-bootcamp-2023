0c0c07661033be20c0df78f2c9fafae1677729fe
rnfor-pro <67124388+rnfor-pro@users.noreply.github.com>
2023-10-04 17:59:51 -0400

    21 restructure root module (#22)
    * Update wee1.md
    
    * #21 change structure of root modules and add tagging to bucket

--- main.tf
+++ main.tf
@@ -1,34 +1,3 @@
-terraform {
-   cloud {
-      organization = "etech-dev"
-
-      workspaces {
-        name = "terra-house-1"
-      }
-    }
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-
-
-provider "aws" {
-  # Configuration options
-}
-
-
-provider "random" {
-  # Configuration options
-}
-
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   length           = 16
@@ -42,12 +11,14 @@ resource "random_string" "bucket_name" {
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
 
-#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
+
+
 
 
 
