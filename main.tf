e3a7cfdc13809fc6ce620f8286e594a3426cf882
rnfor-pro <67124388+rnfor-pro@users.noreply.github.com>
2023-10-04 19:15:56 -0400

    #23 bucket should no longer use random provider (#24)

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
-  lower            = true
-  upper            = false
-  numeric          = false
-  override_special = ""
-}
+# resource "random_string" "bucket_name" {
+#   length           = 16
+#   special          = false
+#   lower            = true
+#   upper            = false
+#   numeric          = false
+#   override_special = ""
+# }
 
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
 
   tags = {
     UserUuid = var.user_uuid
