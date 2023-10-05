8957f33557f385187c98171df5826165f19a3ae3
rnfor-pro <nfor.rudolph1@gmail.com>
2023-10-05 19:40:45 +0000

    #33 invalidate cloudfront tag when content version changes

--- main.tf
+++ main.tf
@@ -16,12 +16,12 @@
 # }
 
 module "terrahouse_aws" {
-  source      = "./modules/terrahouse_aws"
-  user_uuid   = var.user_uuid
-  bucket_name = var.bucket_name
+  source              = "./modules/terrahouse_aws"
+  user_uuid           = var.user_uuid
+  bucket_name         = var.bucket_name
   index_html_filepath = var.index_html_path
   error_html_filepath = var.error_html_path
-  content_version = var.content_version
+  content_version     = var.content_version
 }
 
 
