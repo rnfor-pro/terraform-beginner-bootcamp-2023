019bfaaecdce5a5ad1235936b3a10f0ee5faefb2
rnfor-pro <67124388+rnfor-pro@users.noreply.github.com>
2023-10-05 13:00:38 -0400

    #31 Only upload index.html if a content version is changed (#32)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -36,6 +36,10 @@ resource "aws_s3_object" "index_html" {
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [ etag ]
+  }
 }
 
 resource "aws_s3_object" "error_html" {
@@ -48,6 +52,10 @@ resource "aws_s3_object" "error_html" {
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5(var.error_html_filepath)
+    lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [ etag ]
+  }
 }
 
 
@@ -76,6 +84,9 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   })
 }
 
+resource "terraform_data" "content_version" {
+  input = var.content_version
+}
 
 
 
