000a0adf3fe5d0106baf853a58e06a68bd083e8d
rnfor-pro <67124388+rnfor-pro@users.noreply.github.com>
2023-10-10 09:38:19 -0400

    43 terratowns provider (#45)
    * #43 wip setting up the skeleton for creating custom terraform provider
    
    * #43 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-# terraform {
+terraform {
+    required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
+
+
 #   backend "remote" {
 #     hostname = "app.terraform.io"
 #     organization = "rudolph.life"
@@ -13,16 +21,25 @@
 #         name = "terra-house-1"
 #       }
 #     }
+}
+
+provider "terratowns" {
+  endpoint = "http://localhost:4567/api"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+# module "terrahouse_aws" {
+#   source              = "./modules/terrahouse_aws"
+#   user_uuid           = var.user_uuid
+#   bucket_name         = var.bucket_name
+#   index_html_filepath = var.index_html_path
+#   error_html_filepath = var.error_html_path
+#   content_version     = var.content_version
 # }
 
-module "terrahouse_aws" {
-  source              = "./modules/terrahouse_aws"
-  user_uuid           = var.user_uuid
-  bucket_name         = var.bucket_name
-  index_html_filepath = var.index_html_path
-  error_html_filepath = var.error_html_path
-  content_version     = var.content_version
-}
+
+
 
 
 
